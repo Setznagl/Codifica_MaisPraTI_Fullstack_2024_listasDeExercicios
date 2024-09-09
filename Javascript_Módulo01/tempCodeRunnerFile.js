efd7219cb9a7d7a7904e9aca25ce6cced41e9f47
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-09-09 20:27:17 -0300

    .
    .

--- Javascript_Módulo01/tempCodeRunnerFile.js
+++ Javascript_Módulo01/tempCodeRunnerFile.js
@@ -1,2 +1,4 @@
-    // let valor2EX6 = checaParametros();
-    // let valor3EX6 = checaParametros();
+console.log("// 3. Filtrando Propriedades de Objetos" +
+    "// Objetivo: Dado um objeto produto com várias propriedades, crie uma função que retorna um novo objeto" +
+    "// contendo apenas as propriedades cujo valor seja maior que um valor específico." +
+    "// Use for in para filtrar as propriedades.");
